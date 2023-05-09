9684b295486e5b97bb67e48577c657fd5ca4501b
Conrad <conradbez1@gmail.com>
2023-05-09 18:03:55 +0800

    docs

---
+++ demo/custom_styling.py
@@ -0,0 +1,28 @@
+from hstream import hs
+
+hs.markdown(f"""
+# We're very flexible with styling, should you need it to fine tune your app post MVP
+
+Here's an example of a slider with custom styling 
+
+- move the slider to the center of the page
+
+- set width to 50% of the page
+
+""")
+
+hs.markdown("""
+
+    with hs.doc.tag("center",):
+
+        with hs.html('div',('style', "width: 50%;),):
+        
+            hs.slider("Pick a value", 0,100, 50)
+
+
+""")
+hs.stylesheet_href = hs.list_css_frameworks()["pico.css"]
+
+with hs.doc.tag("center",):
+    with hs.html('div',('style', "width: 50%;"),):
+        hs.slider("Pick a value", 0,100, 50)
