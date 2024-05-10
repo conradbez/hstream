b237c4892c8fc75a458b8d7d1fb88c69b80591be
Conrad <conradbez1@gmail.com>
2024-05-10 11:19:21 +0800

    feat: update cli init

--- demo/example.py
+++ demo/example.py
@@ -1,5 +1,9 @@
 from hstream import hs
+page = hs.nav(["Home", "About"],default_value="Home", key="nav")
 
+if page == "About":
+    hs.markdown("For more info visit [github](github.com/conradbez/hstream)")
+    
 with hs.html("header"):
     hs.markdown(
         f"""## HStream
@@ -23,44 +27,47 @@ with hs.html("section"):
             f"Run with classic web technologies means you can easily customize your app once you need to without rewriting everything",
         )
 
-hs.markdown(
-    f"""
+hs.markdown(f"""
 #Make htmx website creation easy
 Simply write `hs.markdown(2+2)` to get {2+2}
-"""
+""" 
 )
 
 
 hs.markdown(f"""Or create forms like:""")
-with hs.html("form"):
+with hs.html('form'):
     user_number = hs.number_input(
-        "Input a number",
-        default_value=0,
-    )
+            "Input a number",
+            default_value = 0,
+        )
     hs.markdown(f"Your number is {'*even*' if int(user_number) % 2 == 0 else '*odd*'}")
-# with hs.html('header'):
-#     hs.markdown("## HStream also supports displaying plots")
-# with hs.html('section'):
-#     hs.markdown("""
-#     `import matplotlib.pyplot as plt`
+with hs.html('header'):
+    hs.markdown("## HStream also supports displaying plots")
+with hs.html('section'):
+    hs.markdown("""
+    `import matplotlib.pyplot as plt`
 
-#     `import numpy as np`
+    `import numpy as np`
 
-#     `x = np.arange(0,4*np.pi,0.1)   # start,stop,step`
+    `x = np.arange(0,4*np.pi,0.1)   # start,stop,step`
 
-#     `y = np.sin(x) * float(1)`
+    `y = np.sin(x) * float(1)`
 
-#     `fig, ax = plt.subplots()`
+    `fig, ax = plt.subplots()`
 
-#     `ax.plot(x,y)`
+    `ax.plot(x,y)`
 
-#     `hs.pyplot(fig, key='myplot')`
-#     """)
-
-#     import matplotlib.pyplot as plt
-#     import numpy as np
-#     x = np.arange(0,4*np.pi,0.1)   # start,stop,step
-#     y = np.sin(x) * float(1)
-#     fig, ax = plt.subplots()
-#     ax.plot(x,y)
-#     hs.pyplot(fig, key='myplot')
+    `hs.pyplot(fig, key='myplot')`
+    """)
+    try:
+        import matplotlib.pyplot as plt
+        import numpy as np
+        x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+        y = np.sin(x) * float(1)
+        fig, ax = plt.subplots()
+        ax.plot(x,y)
+        hs.pyplot(fig, key='myplot')
+    except Exception as e:
+        hs.markdown(f"hmmm seems you don't have matplotlib installed, please install it with `pip install matplotlib`")
+        raise e
+    
