f0c4223f1c5d9284cce0364e08fce6e481604282
Conrad <conradbez1@gmail.com>
2024-05-12 19:22:05 +0800

    fix: cli error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,22 @@
 import os
 import threading
 from time import sleep
+import subprocess
 
 
 def write_py_script(contents):
     open("./tests/file_to_run.py", "w").write(contents)
 
 
+server_process = None
+
+
 def run_command():
     open("./tests/file_to_run.py", "w").write(" ")
-    os.system("python -m hstream run ./tests/file_to_run.py")
+    global server_process
+    server_process = subprocess.Popen(
+        ["python", "-m", "hstream", "run", "./tests/file_to_run.py"]
+    )
     sleep(2)
 
 
@@ -17,9 +24,11 @@ def pytest_sessionstart(session):
     print("All tests are about to start. Starting server")
     thread = threading.Thread(target=run_command)
     thread.start()
-    return
 
 
 def pytest_sessionfinish(session, exitstatus):
-    os.system("kill -9 $(lsof -t -i:8000)")
+    global server_process
+    if server_process is not None:
+        server_process.terminate()
     print("All tests are finished. Stopping server")
+    os.remove("./tests/file_to_run.py")
