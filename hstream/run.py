d7673d8ae91c42b506a269d498e7ef897d0ff3c0
conradbez <conradbez@users.noreply.github.com>
2024-05-27 07:43:02 +0800

    fix: handle spaces in the path to user script

--- hstream/run.py
+++ hstream/run.py
@@ -10,9 +10,9 @@ def run_server(
 ):
     path_to_hstream_dir = os.path.dirname(os.path.abspath(hstream.__file__))
 
-    os.system(f"python {path_to_hstream_dir}/django_server/manage.py migrate")
+    os.system(f'python "{path_to_hstream_dir}/django_server/manage.py" migrate')
     os.environ["HS_FILE_TO_RUN"] = str(file)
-    command = f"python {path_to_hstream_dir}/django_server/manage.py runserver"
+    command = f'python "{path_to_hstream_dir}/django_server/manage.py" runserver'
     if os.environ.get("PORT", False):
         print(f"found port in env, using port {os.environ['PORT']}")
         command += f" 0.0.0.0:{os.environ['PORT']}"
