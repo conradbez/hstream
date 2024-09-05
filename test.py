6f25ca242ceedfc927954fa8559d54e63aa5a9a1
conrad <cb@example.com>
2024-09-06 03:25:14 +0800

    feat: shoelace multiselect

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ from hstream import hs
 hs.markdown('test')
 
 sl_ms = hs.sl_multiselect(['test1', 'test2', 'test3'], default_value=['test2'])
+sl_ms = hs.sl_button('Shoelace button')
 
 hs.markdown(sl_ms)
+
+hs.button('Normal button')
