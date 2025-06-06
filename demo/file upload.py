877676ecb47df22af9aad80bea0f3838e491c739
conrad <cb@example.com>
2025-06-06 11:28:39 +0800

    feat: upload file ability

---
+++ demo/file upload.py
@@ -0,0 +1,8 @@
+from hstream import hs
+hs.markdown("# upload")
+uploaded_file = hs.file_upload("Upload your file")
+
+if uploaded_file and hs.button("Process and Download"):
+    hs.markdown('uploaded')
+    
+hs.markdown(uploaded_file)
